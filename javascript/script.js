b2b64d71ca83672844d3d1b157dfbcbd09c53fe8
ShahreaAhmed <shahrearuman@gmail.com>
2025-08-29 19:22:01 +0600

    call history compleate

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+// local time set
+const time = new Date();
+const timeString = time.toLocaleTimeString();
+document.getElementById('current-time').innerText = timeString;
+
+
+// heart button call
+document.getElementsByClassName()
